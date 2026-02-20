5a398fffdaee1ed6092871a1ef1beb131b5d1ca6
Samuel Laferriere <9342524+samlaf@users.noreply.github.com>
2026-02-19 22:29:24 -0500

    fix: mercury versioning (#15)
    bump to 0.8.31. also make mercury between prague and osaka, as it is in
    revm and solidity.

--- crates/core/src/utils/mod.rs
+++ crates/core/src/utils/mod.rs
@@ -26,9 +26,6 @@ pub use wd::*;
 /// Extensions acceptable by solc compiler.
 pub const SOLC_EXTENSIONS: &[&str] = &["sol", "yul"];
 
-/// Mercury support (Seismic)
-pub const MERCURY_SOLC: Version = Version::new(0, 8, 28);
-
 /// Support for configuring the EVM version
 /// <https://blog.soliditylang.org/2018/03/08/solidity-0.4.21-release-announcement/>
 pub const BYZANTIUM_SOLC: Version = Version::new(0, 4, 21);
@@ -72,6 +69,18 @@ pub const PRAGUE_SOLC: Version = Version::new(0, 8, 27);
 /// Osaka support
 /// <https://soliditylang.org/blog/2025/03/12/solidity-0.8.29-release-announcement>
 pub const OSAKA_SOLC: Version = Version::new(0, 8, 29);
+
+/// Mercury support (Seismic)
+///
+/// This is a minimum version floor — "Mercury support requires at least 0.8.31".
+/// It should match the upstream version that seismic-solidity was built on.
+/// The latest upstream rebase (to 0.8.31) is: https://github.com/SeismicSystems/seismic-solidity/pull/83
+/// If we ever decide to do another rebase on top of a later solidity version before finalizing the
+/// mercury evm-version feature set, then we will need to update this version.
+///
+/// Note also that despite its solc version being > osaka, in terms of feature sets (see EvmVersion
+/// enum), Mercury currently only enables Ethereum's Prague hardfork, not Osaka.
+pub const MERCURY_SOLC: Version = Version::new(0, 8, 31);
 
 // `--base-path` was introduced in 0.6.9 <https://github.com/ethereum/solidity/releases/tag/v0.6.9>
 pub static SUPPORTS_BASE_PATH: Lazy<VersionReq> =
